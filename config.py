6fc02d45ee238745e49c7dc756c5797e544d50bf
Jialin Ma <marlin@inventati.org>
2019-09-15 18:15:58 -0400

    Add example config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+
+mastodon_credentials = [
+    ("https://gab.com"         , "your_email@xxx.com" , "your_password"),
+    ("https://pawoo.net"       , "your_email@xxx.com" , "your_password"),
+    ("https://mastodon.social" , "your_email@xxx.com" , "your_password"),
+]
