7d5309e1c0815010a91d50cca552580e00adaf4c
Jialin Ma <marlin@inventati.org>
2019-09-15 19:30:11 -0400

    Fix config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 
 mastodon_credentials = [
-    ("https://gab.com"         , "your_email@xxx.com" , "your_password"),
-    ("https://pawoo.net"       , "your_email@xxx.com" , "your_password"),
-    ("https://mastodon.social" , "your_email@xxx.com" , "your_password"),
+    ("pawoo.net"       , "your_email@xxx.com" , "your_password"),
+    ("mastodon.social" , "your_email@xxx.com" , "your_password"),
 ]
